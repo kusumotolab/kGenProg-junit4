d7642fca361c16ee58613c9ae8babbb744c56acb
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2019-01-05 08:53:09 +0900

    reformat

--- src/main/java/org/junit/experimental/KgpDuration.java
+++ src/main/java/org/junit/experimental/KgpDuration.java
@@ -5,12 +5,12 @@ import java.util.concurrent.TimeUnit;
 public class KgpDuration {
     public long timeout;
     public TimeUnit timeUnit;
-    
+
     public KgpDuration(long timeout, TimeUnit timeUnit) {
         this.timeout = timeout;
         this.timeUnit = timeUnit;
     }
-    
+
     public static KgpDuration defaultDuration() {
         return new KgpDuration(0, TimeUnit.SECONDS);
     }
